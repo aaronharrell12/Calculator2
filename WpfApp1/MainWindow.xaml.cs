df3f81995c24e66ae087471ded0ebafc053ebe91
Aaron Harrell <aaronharrell12@gmail.coim>
2020-12-06 19:23:20 -0600

    New Calc

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,13 +21,14 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
         // Variables used in calculator
-        double    current_Value;
-        double        new_value;
-        String new_value_string;
-        String          prev_op;
-        double       temp_value;
-        ArrayList       History;
+            double    current_Value;
+            double        new_value;
+            String new_value_string;
+            String          prev_op;
+            double       temp_value;
+            ArrayList       History;
 
         public MainWindow()
         {
@@ -41,73 +42,84 @@ namespace WpfApp1
                 new_value        =  0; // Represents the number being currently entered
                 new_value_string = ""; // Represents the number being currently entered as a string (because digits)
                 prev_op          = ""; // Represents the previous operation.
-           
-           
-            // Step 2: Loop Through all buttons on the grid
+
+
+            // --------------- (Bind all buttons to a function based off of name)-------------------------------
+
+                // Step 2:  Loop Through all buttons on the grid 
                 for (int i =0; i< grid.Children.Count; i++)
                 {
-                    // Step 2.1: Grab the first button
-                        Button ele        = (Button) grid.Children[i];
+                        // Step 2.1: Grab the button
+                            Button ele        = (Button) grid.Children[i];
 
-                    // Step 2.2: Grab the Buttons name and split it by '-' (example: "operation-mult" ==> ["operation","mult"])
-                        String[] name_arr = ele.Name.Split('_');
+                        // Step 2.2: Grab that Buttons name and split it by '_' (example: "operation_mult" ==> ["operation","mult"])
+                            String[] name_arr = ele.Name.Split('_');
 
-                    // Step 2.3: Grab the first part 
-                        String    name    = name_arr[0];
+                        // Step 2.3: Grab the first part 
+                            String    name    = name_arr[0];
     
-                    // Step 2.4 Check to see if button is a number
-                        if (name.Equals("Number"))
-                        {
-                            ele.Click += new RoutedEventHandler(Numbers_Clicked);
+                        // Step 2.4 Check to see if button is a number
+                            if (name.Equals("Number"))
+                            {
+                                ele.Click += new RoutedEventHandler(Numbers_Clicked);
                     
-                        }
-                    // Step 2.5: Check to see if button is a operation
-                        if (name.Equals("Operation"))
-                        {
+                            }
+                        // Step 2.5: Check to see if button is a operation
+                            if (name.Equals("Operation"))
+                            {
                         
-                            // Step 2.5.1: Grab the Second part
-                                name = name_arr[1];
+                                // Step 2.5.1: Grab the Second part
+                                    name = name_arr[1];
 
-                            // Step 2.5.2: Check to see what type of operation:
-                                switch (name)
-                                {
-                                    case "Bin":
-                                        ele.Click += new RoutedEventHandler(Operations_Clicked);
-                                        break;
-                                    case "Result":
-                                        ele.Click += new RoutedEventHandler(Operations_Res_Clicked);
-                                        break;
-                                    case "clr":
-                                        ele.Click += new RoutedEventHandler(Operations_clr_Clicked);
-                                        break;
+                                // Step 2.5.2: Check to see what type of operation:
+                                    switch (name)
+                                    {
+                                        // binary operations (+,-,/,*)
+                                        case "Bin":
+                                            ele.Click += new RoutedEventHandler(Operations_Clicked);
+                                            break;
+                                        // Equal to operation (=)
+                                        case "Result":
+                                            ele.Click += new RoutedEventHandler(Operations_Res_Clicked);
+                                            break;
+                                        // Clear opeartion
+                                        case "clr":
+                                            ele.Click += new RoutedEventHandler(Operations_clr_Clicked);
+                                            break;
+
+                                }
 
                             }
 
-                        }
-
-                      // Step 2.6: Update the grid button
-                        grid.Children[i] = ele;
+                          // Step 2.6: Update the grid button
+                            grid.Children[i] = ele;
                 }
    
         }
+       
 
+
+        ///     Adds a digit to the number window and updates the temporary value (temp_value) based on:
+        ///         - current_value (based on previous results)
+        ///         - new_value     (based on what is being type in)
         private void Numbers_Clicked(object sender, RoutedEventArgs e)
         {
+
 
             // Step 1: Grab the button being sent
                 Button x             =          (Button) sender;
 
             // Step 2: Grab the text in the calculator
-                String textIn        =           textBlock.Text;
+                String textIn        =       container_num.Text;
 
-            // Step 3: Add an addition digit to the textbox
+            // Step 3: Add an additional digit to the textbox
                 textIn              +=        (String)x.Content;
 
             // Step 4: Add an additional digit to a the new_value string
                 new_value_string    +=        (String)x.Content;
 
             // Step 5: 
-                textBlock.Text       =                   textIn;
+                container_num.Text       =                   textIn;
 
             // Step 6: Check to see what the previous operation was 
                 if (new_value_string.CompareTo("") != 0)
@@ -141,37 +153,41 @@ namespace WpfApp1
 
 
             // TESTING ONLY (CHECKS VALUES)
-            curr_val_box.Text    = current_Value.ToString();
-            new_val_box.Text     =     new_value.ToString();
-            new_val_str_box.Text =         new_value_string;
-            temp_box.Text        =    temp_value.ToString();
-
+                /*
+                curr_val_box.Text    = current_Value.ToString();
+                new_val_box.Text     =     new_value.ToString();
+                new_val_str_box.Text =         new_value_string;
+                temp_box.Text        =    temp_value.ToString();
+                */
 
 
         }
+
+        // When an operation is clicked modify the window
         private void Operations_Clicked(object sender, RoutedEventArgs e)
         {
 
             Button x = (Button)sender;
             
             // Text from user input
-            String textIn = textBlock.Text;
-            Console.WriteLine(textIn);
+            String container_numText = container_num.Text;
+            
+            // Text from history window
+            String history_winText   = History_win.Text;
 
+            String endPiece = container_numText.Substring(Math.Max(0, container_numText.Length - 1));
 
-
-            String endPiece = textIn.Substring(Math.Max(0, textIn.Length - 1));
             if (int.TryParse(endPiece, out int empty))
             {
-                textIn = "(" + textIn + ")";
+                container_numText =  "0";
             }
             else
             {
-                textIn = textIn.Remove(textIn.Length - 1);
+                container_numText = container_numText.Remove(container_numText.Length - 1);
             }
 
-            textIn                 +=        (String)x.Content;
-            textBlock.Text          =                   textIn;
+            container_numText      +=        (String)x.Content;
+            container_num.Text      =        container_numText;
 
             // CHANGE STATE VARIABLES
             prev_op                 =        (String)x.Content;
@@ -179,20 +195,21 @@ namespace WpfApp1
             new_value_string        =                       "";
             new_value               =                        0;
             temp_value              =                        0;
-
+             
             // TESTING
+            /*
             curr_val_box.Text       = current_Value.ToString();
             new_val_box.Text        =     new_value.ToString();
             new_val_str_box.Text    =         new_value_string;
             temp_box.Text           =    temp_value.ToString();
-
+            */
         }
         private void Operations_Res_Clicked(object sender, RoutedEventArgs e)
         {
 
             
             // 
-            textBlock.Text        =    temp_value.ToString(); // Sets the calculator textbox to the temp value 
+            container_num.Text        =    temp_value.ToString(); // Sets the calculator textbox to the temp value 
             
             
             // CHANGE STATE VARIABLES 
@@ -201,11 +218,12 @@ namespace WpfApp1
             new_value_string      =    temp_value.ToString(); // Sets the new value string to temp
 
             // TESTING 
+            /*
             curr_val_box.Text     = current_Value.ToString();
             new_val_box.Text      =     new_value.ToString();
             temp_box.Text         =    temp_value.ToString();
             new_val_str_box.Text  =         new_value_string;
-
+            */
         }
 
         private void Operations_clr_Clicked(object sender, RoutedEventArgs e)
@@ -216,17 +234,32 @@ namespace WpfApp1
             temp_value           =                        0;
             new_value_string     =                       "";
             prev_op              =                       "";
-            textBlock.Text       =                      "0";
+            container_num.Text   =                      "0";
 
 
             // TESTING
+            /*
             curr_val_box.Text    = current_Value.ToString();
             new_val_box.Text     =     new_value.ToString();
             new_val_str_box.Text =         new_value_string;
             temp_box.Text        =    temp_value.ToString();
+            */
         }
 
         private void Operation_Bin_mul_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+        private void drawFunction(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        // Useless function [DONT DELETE]
+        private void nothing(object sender, RoutedEventArgs e)
         {
 
         }
