4b6a568fde9e0e4f8b5bc4c8e302bbf2b8d6e87e
Aaron Harrell <aaronharrell12@gmail.coim>
2020-12-10 23:36:05 -0600

    Newest version

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,80 +23,83 @@ namespace WpfApp1
     {
 
         // Variables used in calculator
-            double    current_Value;
-            double        new_value;
-            String new_value_string;
-            String          prev_op;
-            double       temp_value;
-            ArrayList       History;
-
+        double current_Value;
+        double new_value;
+        String new_value_string;
+        bool prev_op;
+        double temp_value;
+        ArrayList History;
+        bool res_op;
+        StringToFormula stf;
         public MainWindow()
         {
-            History          = new ArrayList();
+            History = new ArrayList();
+            stf = new StringToFormula();
             InitializeComponent();
 
 
             // Step 1: Initialize all variables.
-                current_Value    =  0; // Represents the current value being held. 
-                temp_value       =  0; // Represents a temporary value for new operations
-                new_value        =  0; // Represents the number being currently entered
-                new_value_string = ""; // Represents the number being currently entered as a string (because digits)
-                prev_op          = ""; // Represents the previous operation.
+            current_Value = 0; // Represents the current value being held. 
+            temp_value = 0; // Represents a temporary value for new operations
+            new_value = 0; // Represents the number being currently entered
+            new_value_string = ""; // Represents the number being currently entered as a string (because digits)
+            prev_op = false; // Represents the previous operation.
+            res_op = true;
 
 
             // --------------- (Bind all buttons to a function based off of name)-------------------------------
 
-                // Step 2:  Loop Through all buttons on the grid 
-                for (int i =0; i< grid.Children.Count; i++)
+            // Step 2:  Loop Through all buttons on the grid 
+            for (int i = 0; i < grid.Children.Count; i++)
+            {
+                // Step 2.1: Grab the button
+                Button ele = (Button)grid.Children[i];
+
+                // Step 2.2: Grab that Buttons name and split it by '_' (example: "operation_mult" ==> ["operation","mult"])
+                String[] name_arr = ele.Name.Split('_');
+
+                // Step 2.3: Grab the first part 
+                String name = name_arr[0];
+                
+                // Step 2.4 Check to see if button is a number
+                if (name.Equals("Number"))
                 {
-                        // Step 2.1: Grab the button
-                            Button ele        = (Button) grid.Children[i];
+                    ele.Click += new RoutedEventHandler(Numbers_Clicked);
 
-                        // Step 2.2: Grab that Buttons name and split it by '_' (example: "operation_mult" ==> ["operation","mult"])
-                            String[] name_arr = ele.Name.Split('_');
-
-                        // Step 2.3: Grab the first part 
-                            String    name    = name_arr[0];
-    
-                        // Step 2.4 Check to see if button is a number
-                            if (name.Equals("Number"))
-                            {
-                                ele.Click += new RoutedEventHandler(Numbers_Clicked);
-                    
-                            }
-                        // Step 2.5: Check to see if button is a operation
-                            if (name.Equals("Operation"))
-                            {
-                        
-                                // Step 2.5.1: Grab the Second part
-                                    name = name_arr[1];
-
-                                // Step 2.5.2: Check to see what type of operation:
-                                    switch (name)
-                                    {
-                                        // binary operations (+,-,/,*)
-                                        case "Bin":
-                                            ele.Click += new RoutedEventHandler(Operations_Clicked);
-                                            break;
-                                        // Equal to operation (=)
-                                        case "Result":
-                                            ele.Click += new RoutedEventHandler(Operations_Res_Clicked);
-                                            break;
-                                        // Clear opeartion
-                                        case "clr":
-                                            ele.Click += new RoutedEventHandler(Operations_clr_Clicked);
-                                            break;
-
-                                }
-
-                            }
-
-                          // Step 2.6: Update the grid button
-                            grid.Children[i] = ele;
                 }
-   
+                // Step 2.5: Check to see if button is a operation
+                if (name.Equals("Operation"))
+                {
+
+                    // Step 2.5.1: Grab the Second part
+                    name = name_arr[1];
+
+                    // Step 2.5.2: Check to see what type of operation:
+                    switch (name)
+                    {
+                        // binary operations (+,-,/,*)
+                        case "Bin":
+                            ele.Click += new RoutedEventHandler(Operations_Clicked);
+                            break;
+                        // Equal to operation (=)
+                        case "Result":
+                            ele.Click += new RoutedEventHandler(Operations_Res_Clicked);
+                            break;
+                        // Clear opeartion
+                        case "clr":
+                            ele.Click += new RoutedEventHandler(Operations_clr_Clicked);
+                            break;
+
+                    }
+
+                }
+
+                // Step 2.6: Update the grid button
+                grid.Children[i] = ele;
+            }
+
         }
-       
+
 
 
         ///     Adds a digit to the number window and updates the temporary value (temp_value) based on:
@@ -107,59 +110,42 @@ namespace WpfApp1
 
 
             // Step 1: Grab the button being sent
-                Button x             =          (Button) sender;
+            Button x = (Button)sender;
 
             // Step 2: Grab the text in the calculator
-                String textIn        =       container_num.Text;
+            String textIn = container_num.Text;
 
-            // Step 3: Add an additional digit to the textbox
-                textIn              +=        (String)x.Content;
+            if ( !((String)x.Content).Equals(".") || !textIn.Contains(".") ) {
+                if (textIn.Equals("0") || prev_op || res_op)
+                {
+                    textIn = (String)x.Content;
+                }
 
-            // Step 4: Add an additional digit to a the new_value string
-                new_value_string    +=        (String)x.Content;
+                else
+                {
+                    // Step 3: Add an additional digit to the textbox
+                    textIn += (String)x.Content;
+                }
+
+            }
+
+            prev_op = false;
+            res_op = false;
+
 
             // Step 5: 
-                container_num.Text       =                   textIn;
+            container_num.Text = textIn;
 
-            // Step 6: Check to see what the previous operation was 
-                if (new_value_string.CompareTo("") != 0)
-                {
-                    // Step 6.1: Grab the current new value.
-                        new_value        = Double.Parse(new_value_string);
-
-                    // Step 6.2: Check the previous operation and compute the temporary result
-                        switch (prev_op){
-
-                            case "=":
-                            case  "":
-                                temp_value = new_value;
-                                break;
-                            case "+":
-                                temp_value = new_value     + current_Value;
-                                break;
-                            case "-":
-                                temp_value = current_Value -     new_value;
-                                break;
-                            case "*":
-                                temp_value = current_Value *     new_value;
-                                break;
-                            case "/":
-                                temp_value = current_Value /     new_value;
-                                break;
-                        }
-
-                
-                }
 
 
             // TESTING ONLY (CHECKS VALUES)
-                /*
-                curr_val_box.Text    = current_Value.ToString();
-                new_val_box.Text     =     new_value.ToString();
-                new_val_str_box.Text =         new_value_string;
-                temp_box.Text        =    temp_value.ToString();
-                */
-
+            /*
+            curr_val_box.Text    = current_Value.ToString();
+            new_val_box.Text     =     new_value.ToString();
+            new_val_str_box.Text =         new_value_string;
+            temp_box.Text        =    temp_value.ToString();
+            */
+            
 
         }
 
@@ -168,34 +154,40 @@ namespace WpfApp1
         {
 
             Button x = (Button)sender;
-            
+
             // Text from user input
             String container_numText = container_num.Text;
-            
+
             // Text from history window
             String history_winText   = History_win.Text;
 
-            String endPiece = container_numText.Substring(Math.Max(0, container_numText.Length - 1));
 
-            if (int.TryParse(endPiece, out int empty))
+            if (prev_op)
             {
-                container_numText =  "0";
+                history_winText = history_winText.Substring(0, history_winText.Length - 1)+ (String)x.Content;
+
             }
             else
             {
-                container_numText = container_numText.Remove(container_numText.Length - 1);
+
+                // Adds additional operations and values to the history container
+                history_winText += container_numText + (String)x.Content;
+
+
+                // Updates the history container
+               
+
             }
-
-            container_numText      +=        (String)x.Content;
-            container_num.Text      =        container_numText;
-
+            History_win.Text = history_winText;
+            prev_op = true;
+            
             // CHANGE STATE VARIABLES
-            prev_op                 =        (String)x.Content;
-            current_Value           =               temp_value;
-            new_value_string        =                       "";
-            new_value               =                        0;
-            temp_value              =                        0;
-             
+            //prev_op = (String)x.Content;
+            //current_Value = temp_value;
+            //new_value_string = "";
+            //new_value = 0;
+            //temp_value = 0;
+
             // TESTING
             /*
             curr_val_box.Text       = current_Value.ToString();
@@ -207,15 +199,16 @@ namespace WpfApp1
         private void Operations_Res_Clicked(object sender, RoutedEventArgs e)
         {
 
-            
+
             // 
-            container_num.Text        =    temp_value.ToString(); // Sets the calculator textbox to the temp value 
-            
-            
+            container_num.Text = stf.Eval(History_win.Text+container_num.Text).ToString(); // Sets the calculator textbox to the temp value 
+            History_win.Text = "";
+            res_op = true;
+
             // CHANGE STATE VARIABLES 
-            new_value             =                        0; // Sets the new value to 0.
-            prev_op               =                      "="; // Previous operation is resultant
-            new_value_string      =    temp_value.ToString(); // Sets the new value string to temp
+            new_value = 0; // Sets the new value to 0.
+            prev_op = false; // Previous operation is resultant
+            new_value_string = temp_value.ToString(); // Sets the new value string to temp
 
             // TESTING 
             /*
@@ -229,13 +222,13 @@ namespace WpfApp1
         private void Operations_clr_Clicked(object sender, RoutedEventArgs e)
         {
             // RESET ALL VALUES TO DEFAULT
-            current_Value        =                        0;
-            new_value            =                        0;
-            temp_value           =                        0;
-            new_value_string     =                       "";
-            prev_op              =                       "";
-            container_num.Text   =                      "0";
-
+           // current_Value = 0;
+            //new_value = 0;
+            //temp_value = 0;
+            //new_value_string = "";
+            //prev_op = "";
+            container_num.Text = "0";
+            History_win.Text = "";
 
             // TESTING
             /*
@@ -248,7 +241,7 @@ namespace WpfApp1
 
         private void Operation_Bin_mul_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
 
---
+++ WpfApp1/StringToFormula.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+
+        public class StringToFormula
+        {
+            private string[] _operators = { "-", "+", "/", "*", "^", "%" };
+            private string[] _mono_operators = { "sin", "cos", "log","abs","ln","pm"};
+            private string[] _constants = { "e", "pi" };
+            private Func<double, double, double>[] _operations = {
+                (a1, a2) => a1 - a2,
+                (a1, a2) => a1 + a2,
+                (a1, a2) => a1 / a2,
+                (a1, a2) => a1 * a2,
+                (a1, a2) => Math.Pow(a1, a2)
+
+            };
+            private Func<double, double>[] _mono_operations = {
+                (a1)     => Math.Sin(a1),
+                (a1)     => Math.Cos(a1),
+                (a1)     => Math.Log(a1),
+                (a1)     => Math.Abs(a1),
+                (a1)     => Math.Log(a1),
+                (a1)     => -a1
+            };
+
+            public double Eval(string expression)
+            {
+                List<string>  tokens            = getTokens(expression);//  ["(", "2","+","45",")"]
+                Stack<double> operandStack      =   new Stack<double>();//  ["2","45"]
+                Stack<string> operatorStack     =   new Stack<string>();//  ["+"]
+                Stack<string> monooperatorStack =   new Stack<string>();
+                bool next = false;
+                int tokenIndex = 0;
+
+                while (tokenIndex < tokens.Count)
+                {
+                    string token = tokens[tokenIndex];
+                    if (token == "(")
+                    {
+                        string subExpr = getSubExpression(tokens, ref tokenIndex);
+                        operandStack.Push(Eval(subExpr));
+                        next = true;
+                        continue;
+                    }
+                    if (token == ")")
+                    {
+                        throw new ArgumentException("Mis-matched parentheses in expression");
+                    }
+
+
+                    //If this is an operator  
+                    if (Array.IndexOf(_mono_operators, token) >= 0)
+                    {
+
+                        monooperatorStack.Push(token);
+
+                    }
+                    else if (Array.IndexOf(_operators, token) >= 0)
+                    {
+                        if (monooperatorStack.Count > 0)
+                        {
+                            throw new ArgumentException("Binary Operator following a Monomial operator");
+                        }
+                        while (operatorStack.Count > 0 && Array.IndexOf(_operators, token) < Array.IndexOf(_operators, operatorStack.Peek()))
+                        {
+                            string op = operatorStack.Pop();
+                            double arg2 = operandStack.Pop();
+                            double arg1 = operandStack.Pop();
+                            operandStack.Push(_operations[Array.IndexOf(_operators, op)](arg1, arg2));
+                        }
+                        operatorStack.Push(token);
+                        next = false;
+                    }
+                    else
+                    {
+                        operandStack.Push(double.Parse(token));
+                        next = true;
+                    }
+
+
+                    if (next)
+                    {
+
+                        while (monooperatorStack.Count > 0)
+                        {
+                            string mono_op = monooperatorStack.Pop();
+                            double arg1 = operandStack.Pop();
+                            operandStack.Push(_mono_operations[Array.IndexOf(_mono_operators, mono_op)](arg1));
+                        }
+                        next = false;
+                    }
+                    tokenIndex += 1;
+                }
+
+
+
+            if (next)
+            {
+
+                while (monooperatorStack.Count > 0)
+                {
+                    string mono_op = monooperatorStack.Pop();
+                    double arg1 = operandStack.Pop();
+                    operandStack.Push(_mono_operations[Array.IndexOf(_mono_operators, mono_op)](arg1));
+                }
+                next = false;
+            }
+
+            while (operatorStack.Count > 0)
+                {
+                    string op = operatorStack.Pop();
+                    double arg2 = operandStack.Pop();
+                    double arg1 = operandStack.Pop();
+                    operandStack.Push(_operations[Array.IndexOf(_operators, op)](arg1, arg2));
+                }
+                return operandStack.Pop();
+            }
+
+            private string getSubExpression(List<string> tokens, ref int index)
+            {
+                StringBuilder subExpr = new StringBuilder();
+                int parenlevels = 1;
+                index += 1;
+                while (index < tokens.Count && parenlevels > 0)
+                {
+                    string token = tokens[index];
+                    if (tokens[index] == "(")
+                    {
+                        parenlevels += 1;
+                    }
+
+                    if (tokens[index] == ")")
+                    {
+                        parenlevels -= 1;
+                    }
+
+                    if (parenlevels > 0)
+                    {
+                        subExpr.Append(token);
+                    }
+
+                    index += 1;
+                }
+
+                if ((parenlevels > 0))
+                {
+                    throw new ArgumentException("Mis-matched parentheses in expression");
+                }
+                return subExpr.ToString();
+            }
+
+            public List<string> getTokens(string expression)
+            {
+                string operators = "()^*/+-";
+                List<string> tokens = new List<string>();
+                StringBuilder sb = new StringBuilder();
+                bool wasNumber = false;
+   
+                int i = 0;
+                foreach (char c in expression.Replace(" ", string.Empty))
+                {
+
+                    // Finds operator
+                    if (operators.IndexOf(c) >= 0)
+                    {
+                        // Checks to see if there are digits 
+                        if ((sb.Length > 0))
+                        {
+                            tokens.Add(sb.ToString());
+                            sb.Length = 0;
+                        }
+
+                        // Adds Operator
+                        tokens.Add(c.ToString());
+
+                        wasNumber = false;
+
+                    }
+
+                    // Finds Character
+                    else if (Char.IsLetter(c))
+                    {
+
+                        // 3sin  --->  3 * sin
+                        if (wasNumber)
+                        {
+                            tokens.Add(sb.ToString());
+                            tokens.Add("*");
+                            sb.Length = 0;
+                            wasNumber = false;
+                        }
+
+                        sb.Append(Char.ToLower(c));
+
+                        if (Array.IndexOf(_mono_operators, sb.ToString()) >= 0)
+                        {
+                            tokens.Add(sb.ToString());
+                            sb.Length = 0;
+                        }
+
+                    }
+
+                    // finds number
+                    else
+                    {
+                        if (sb.Length > 0 && !wasNumber)
+                        {
+                            throw new ArgumentException("Non Operator found in expression.");
+                        }
+
+                        sb.Append(c);
+                        wasNumber = true;
+                    }
+                }
+
+                // Adds remaining digits as number
+                if ((sb.Length > 0))
+                {
+                    tokens.Add(sb.ToString());
+                }
+                return tokens;
+            }
+        }
+    
+}
